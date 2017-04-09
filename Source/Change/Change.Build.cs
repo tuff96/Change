574b7aa1af4e6e106d54520c0a8ec9f87b38e40e
Tufis Alex <dadya1996@gmail.com>
2017-04-09 02:45:15 +0200

    For the horde

--- Source/Change/Change.Build.cs
+++ Source/Change/Change.Build.cs
@@ -6,9 +6,9 @@ public class Change : ModuleRules
 {
 	public Change(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
---
+++ Source/Change/Public/ItemBase.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Runtime/UMG/Public/Components/Image.h"
+#include "ItemBase.generated.h"
+/**
+ * 
+ */
+UCLASS(BlueprintType,  Blueprintable)
+class CHANGE_API UItemBase:public UObject
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UImage ItemTitle;
+};
